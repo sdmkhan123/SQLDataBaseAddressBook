a8d10cb7d068fcc768307cfe915fb9255e9018cc
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-21 00:27:17 +0530

    Create Address Book Service Data Base Table

--- AddressBookServiceDataBase.sql
+++ AddressBookServiceDataBase.sql
@@ -2,3 +2,16 @@
 create database AddressBookServiceDB;
 -- View database name
 use AddressBookServiceDB;
+select DB_NAME()
+--UC-2: Create table in the database
+create table Address_Book
+(
+FirstName varchar(25) not null,
+LastName varchar(25) not null,
+Address varchar(60) not null,
+City varchar(15) not null,
+State varchar(15) not null,
+Zipcode varchar(6) not null,
+PhoneNumber varchar(12) not null,
+Email varchar(25) not null
+);
